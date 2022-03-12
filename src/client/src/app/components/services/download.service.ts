38605333ffdc00641e8cbc236f730bcce80b25b7
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-03-12 00:30:02 -0300

    update tranlation and add new team members

--- src/client/src/app/components/services/download.service.ts
+++ src/client/src/app/components/services/download.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from "../../../environments/environment";
 
 @Injectable({
@@ -13,11 +13,27 @@ export class DownloadService {
   constructor(private httpClient: HttpClient) {}
 
   downloadRequest(parameters): Observable<Blob> {
-    return this.httpClient.post(this.apiURL, parameters, { responseType: 'blob' })
+    return this.httpClient.post(this.apiURL, parameters, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
+      }),
+      responseType: 'blob'
+    });
   }
 
   downloadRequestSLD(layer): Observable<Blob> {
     const url = `${environment.OWS}/ows?request=GetStyles&layers=${layer}&service=wms&version=1.1.1`;
-    return this.httpClient.get(url, { responseType: 'blob' })
+    return this.httpClient.get(url, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
+      }),
+      responseType: 'blob'
+    })
   }
 }
