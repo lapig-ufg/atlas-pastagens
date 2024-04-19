5d90506c610ab1586d338bd35ec6431fc405b26a
James-jamames <tiagogonsalves2001@gmail.com>
2024-04-19 10:39:12 -0300

    Update Angualr cdk & material 14.2.7 -> 15.2.9

--- src/client/src/app/components/right-side-bar/right-side-bar.component.ts
+++ src/client/src/app/components/right-side-bar/right-side-bar.component.ts
@@ -10,7 +10,7 @@ import {
 } from '@angular/core';
 import { LocalizationService } from "../../@core/internationalization/localization.service";
 import { ChartService } from '../services/charts.service';
-import { MatDialog } from '@angular/material/dialog';
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 import { CustomerService } from '../services/customer.service';
 import { Descriptor, Layer, Legend, Menu } from "../../@core/interfaces";
 import Map from 'ol/Map';
